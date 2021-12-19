16b07911aab7bd16ba6e00e76f0fcbe0db1c0c09
Maksim <narushevich.maksim@gmail.com>
2021-12-19 19:01:14 +0300

    Added JWT Token support

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Exceptions\AuthenticationException;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            throw new AuthenticationException(implode(',', $validator->messages()->all()));
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if ($token = auth()->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            throw new AuthenticationException(implode(',', $validator->messages()->all()));
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = auth()->claims(['user_id' => $user->_id])->login($user);
+        return $this->respondWithToken($token);
+    }
+
+    public function logout(Request $request){
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+
+    protected function respondWithToken(string $token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->guard()->factory()->getTTL() * 60,
+            'user' => auth()->user(),
+        ]);
+    }
+}
