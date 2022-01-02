7568e1e3d1af017b446db5720ad7414b7818fcb1
Maksim <narushevich.maksim@gmail.com>
2022-01-02 19:01:42 +0300

    Updated unit tests for User and Post models

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use Tests\TestCase;
 
 class PostsTest extends TestCase
@@ -25,12 +26,37 @@ class PostsTest extends TestCase
     }
 
     /** @test */
+    public function auth_user_can_get_list_of_users()
+    {
+        //Create additionally 3 new posts
+        Post::factory(3)->create();
+        $data = $this->getJson(self::POSTS_ENDPOINT);
+        $this->assertCount(3, $data->decodeResponseJson()->json()['data']);
+
+        $data
+            ->assertStatus(200)
+            ->assertSee('data')
+            ->assertJsonFragment(['type' => 'posts'])
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'type',
+                        'attributes' => [
+                            'slug', 'title', 'description', 'user_id', 'created_at', 'updated_at'
+                        ]
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
     public function aut_user_can_see_specific_post()
     {
         $post = $this->createPost();
-
-        $postResponse =  $this->getJson(sprintf('%s/%s', self::POSTS_ENDPOINT, $post->getOriginalContent()['data']->id));
-
+        $postResponse = $this->getJson(
+            sprintf('%s/%s', self::POSTS_ENDPOINT, $post->getOriginalContent()['data']->id)
+        );
 
         $postResponse->assertStatus(200)
             ->assertJsonStructure([
@@ -44,5 +70,34 @@ class PostsTest extends TestCase
 
         $this->assertEquals($postResponse->getOriginalContent()->user_id, $this->user->id);
         $this->assertEquals($postResponse->getOriginalContent()->title, 'Test Post');
+    }
+
+    /** @test */
+    public function auth_user_can_delete_specific_post()
+    {
+        $post = $this->createPost();
+        $postResponse = $this->getJson(sprintf('%s/%s', self::POSTS_ENDPOINT, $post->getOriginalContent()['data']->id));
+
+        $postResponse->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'title',
+                    'slug',
+                    'description'
+                ]
+            ]);
+        $this->assertEquals($postResponse->getOriginalContent()->user_id, $this->user->id);
+        $this->assertEquals($postResponse->getOriginalContent()->title, 'Test Post');
+
+        $this->deleteJson(sprintf('%s/%s', self::POSTS_ENDPOINT, $post->getOriginalContent()['data']->id))
+            ->assertStatus(200)
+            ->assertJsonFragment(
+                ['message' => sprintf('Post with ID %s was deleted', $post->getOriginalContent()['data']->id)]
+            );
+
+        $this
+            ->getJson(sprintf('%s/%s', self::POSTS_ENDPOINT, $post->getOriginalContent()['data']->id))
+            ->assertNotFound();
     }
 }
--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -17,7 +17,7 @@ class UsersTest extends TestCase
     /** @test */
     public function auth_user_can_get_list_of_users()
     {
-        //Create additionally +3 new user
+        //Create additionally +3 new users
         User::factory(3)->create();
         $data = $this->getJson(self::USERS_ENDPOINT);
         $this->assertCount(4, $data->decodeResponseJson()->json()['data']);
@@ -51,5 +51,30 @@ class UsersTest extends TestCase
                 ]
             ])
             ->assertJsonFragment(['email' => self::TEST_USER_EMAIL]);
+    }
+
+    /** @test */
+    public function auth_user_can_delete_specific_post()
+    {
+        $user = User::where('email', self::TEST_USER_EMAIL)->first();
+        $userResponse = $this->getJson(sprintf('%s/%s', self::USERS_ENDPOINT, $user->id));
+
+        $userResponse->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id', 'email'
+                ]
+            ]);
+        $this->assertEquals($userResponse->getOriginalContent()->email,self::TEST_USER_EMAIL);
+
+        $this->deleteJson(sprintf('%s/%s', self::USERS_ENDPOINT, $user->id))
+            ->assertStatus(200)
+            ->assertJsonFragment(
+                ['message' => sprintf('User with ID %s was deleted', $user->id)]
+            );
+
+        $this
+            ->getJson(sprintf('%s/%s', self::USERS_ENDPOINT, $user->id))
+            ->assertNotFound();
     }
 }
